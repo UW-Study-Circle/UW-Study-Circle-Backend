f5ad3b715565a09a97b5deaa03509a0aaa027ddf
Arshad Habib <47796998+arshadmhabib@users.noreply.github.com>
2021-03-22 20:21:33 -0400

    Implemented Create Group Functionality

--- project/main.py
+++ project/main.py
@@ -18,51 +18,26 @@ def profile():
 def signup():
     return render_template('signup.html')
 
-@auth.route('/signup', methods=['GET,'POST'])
-def signup_post():
-
-    firstname = request.form.get('First Name')
-    lastname = request.form.get('Last Name')
-    email = request.form.get('Email')
-    username = request.form.get('Username')
-    password = request.form.get('Password')
-    phonenumber = request.form.get('Phone Number')
-    gender = request.form.get('Gender')
-
-
-    print(request.form)
-    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
-
-    if user: # if a user is found, we want to redirect back to signup page so user can try again  
-        flash('Email address already exists')
-        return redirect(url_for('auth.signup'))
-    # Check the username before creating new user
-    q = User.query.filter_by(username=username)
-    if user: # if a username for a user is found, alert with message
-        flash('This username has already been taken.')
-    # Check password before creating new user
-    # Conditions for checking: The password should be at least 6 characters long 
-    # have 1 upper case letter, 1 lower case letter, 1 number and 1 punctuation.
-    while True:
-        if not any(p.islower() for p in password): 
-            flash("This is NOT a valid password")
-        elif not any(p.isupper() for p in password): 
-            flash("This is NOT a valid password")
-        elif not any(p.isdigit() for p in password):
-            flash("This is NOT a valid password")
-        elif len(password) < 6:
-            flash("This is NOT a valid password")
-        else:
-            flash("This is a valid password")
-            break
-    # create new user with the form data. Hash the password so plaintext version isn't saved.
-    new_user = User(email=email, firstname=firstname, lastname = lastname, username = username
-                password=generate_password_hash(password, method='sha256'),
-                address = address,
-                phonenumber = phonenumber
-                gender = gender
-                )
-
-    # add the new user to the database
-    db.session.add(new_user)
+@main.route('/creategroup')
+@login_required
+def creategroup():
+    groupname = request.form.get('Group Name')
+    description = request.form.get('Group Description')
+    courseInfo = request.form.get('UW-Madison Course Name & Number')
+    capacity = request.form.get('Group Capacity')
+    duration = request.form.get('Period of Study')
+    status = request.form.get('Status')
+    groupname_exist = Group.query.filter_by(groupname=groupname).first() 
+    if groupname:
+        flash('Group name already exists. Please enter a new group name.')
+        # add the new user to the database
+    new_group = Group(groupname=groupname, email=email, description=description, courseInfo=courseInfo, capacity=capacity,
+    duration=duration, status=status)
+    db.session.add(new_group)
     db.session.commit()
+    return render_template('creategroup.html')
+
+@main.route('/viewallgroups')
+@login_required
+def viewallgroups():
+    allGroups = Group.query.all();
