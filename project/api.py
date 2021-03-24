438875b77c65dbc0dc2b13fcf8ba74bb362b7629
akshatgit <akshat14132@iiitd.ac.in>
2021-03-24 16:53:45 -0500

    apispec doc

--- project/api.py
+++ project/api.py
@@ -1,18 +1,27 @@
 from flask import Response, request, jsonify
 from flask_restful import Resource
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_apispec import doc, use_kwargs
+from flask_apispec.views import MethodResource
 
 from models import User, UserSchema
 user_schema = UserSchema()
 
-
-class HelloAPI(Resource):
+class HelloAPI(MethodResource, Resource):
+    @doc(description='Hello World API.', tags=['Hello-World'])
     def get(self):
-        # Add logic to find the user using username/email
+        '''
+        Get method represents a Hello World GET API method
+        '''
         return {'hello': 'world'}
 
-class UserAPI(Resource):
+class UserAPI(MethodResource, Resource):
+    @doc(description='Post request for signup feature.', tags=['User'])
+    @use_kwargs(UserSchema)
     def post(self):
+        '''
+        Post method for User Signup
+        '''
         body = request.get_json()
         if body is None:
             return jsonify({"Error": "Data not in correct format"})
@@ -28,7 +37,8 @@ class UserAPI(Resource):
             gender = body["gender"]
             bday = body["bday"]
             phone = body["phone"]         
-                
+            
+            # Check if a user exists with the give username/email
             email_exist = User.query.filter_by(email=email).first() 
             name_exist = User.query.filter_by(username=username).first() 
             if email_exist:
@@ -54,7 +64,11 @@ class UserAPI(Resource):
             error["Error"] = str(e)
             return jsonify(error)
 
+    @doc(description='Get request for login feature.', tags=['User'])
     def get(self, email):
+        '''
+        Get method for User Login
+        '''
         user = User.query.filter_by(email=email).first()
         result = dict()
         if user is None:
@@ -64,7 +78,11 @@ class UserAPI(Resource):
         # print(type(result))
         return jsonify(result)
 
+    @doc(description='Delete request for delete account feature.', tags=['User'])
     def delete(self, id):
+        '''
+        Delete method for User deletion
+        '''
         user = User.query.filter_by(id=id).first()
         result = dict()
         if user is None:
--- project/server.py
+++ project/server.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import timedelta
 from flask_mail import Mail
 from flask_restful import Api
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from flask_apispec.extension import FlaskApiSpec
 import os
 
 
@@ -13,7 +16,25 @@ db = SQLAlchemy()
 
 app = Flask(__name__)
 
+app.config.update({
+    'APISPEC_SPEC': APISpec(
+        title='Awesome Project',
+        version='v1',
+        plugins=[MarshmallowPlugin()],
+        openapi_version='2.0.0'
+    ),
+    'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON
+    'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'  # URI to access UI of API Doc
+})
 
+# app.config['APISPEC_SPEC'] =  APISpec(
+#         title='Awesome Project',
+#         version='v1',
+#         plugins=[MarshmallowPlugin()],
+#         openapi_version='2.0.0'
+#     )
+# app.config['APISPEC_SWAGGER_URL'] = '/swagger/',  # URI to access API Doc JSON 
+# app.config['APISPEC_SWAGGER_UI_URL'] = '/swagger-ui/'  # URI to access UI of API Doc
 app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite' 
 app.config['REMEMBER_COOKIE_DURATION'] = timedelta(seconds=1800)
@@ -33,9 +54,15 @@ with app.app_context():
 
 from api import *
 
+docs = FlaskApiSpec(app)
 api = Api(app)
-api.add_resource(HelloAPI, '/')
-api.add_resource(UserAPI, '/api/user/', endpoint="create_user")
-api.add_resource(UserAPI, '/api/user/email/<email>', endpoint="search_user")
-api.add_resource(UserAPI, '/api/user/id/<id>', endpoint="delete_user")
+api.add_resource(HelloAPI, '/', endpoint="helloapi")
+docs.register(HelloAPI)
 
+api.add_resource(UserAPI, '/api/user/', endpoint="create_user", methods=['POST'])
+api.add_resource(UserAPI, '/api/user/email/<email>', endpoint="search_user", methods=['GET'])
+api.add_resource(UserAPI, '/api/user/id/<id>', endpoint="delete_user", methods=['DELETE'])
+
+docs.register(UserAPI, endpoint="create_user")
+docs.register(UserAPI, endpoint="search_user")
+docs.register(UserAPI, endpoint="delete_user")
